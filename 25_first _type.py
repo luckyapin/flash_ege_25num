f612e1c684829f61a70fc9a22d73fa2ff93b6035
luckyapin <a.b.gorlov@gmail.com>
2022-07-26 10:23:10 +0300

    Update 25_first _type.py

--- 25_first _type.py
+++ 25_first _type.py
@@ -1,15 +1,9 @@
-
 from itertools import product
-
-
 # длина посл-ти в @ должна быть до 3 т.к. 5 цифр уже есть, а нам нужно меньше 9
-for lenght in range(1, 5):
-
+for lenght in range(1, 4):
     # перебираем все последовательности для @
     for k in product('02468', repeat=lenght):
-
         for dollar in range(1, 10, 2):  # все нечетные
-
             at = ''.join(k)
             number = int('4'+at+'3'+str(dollar)+'79')
             if number % 73 == 0 and number < 10**8:
